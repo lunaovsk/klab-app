cc46db8f6208014b6ae6e7ea5130f495e4d95988
lunaovsk <matheusluna151634@mail.com>
2026-09-15 11:12:19 -0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/klab/app/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.klab.app
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/jvmMain/kotlin/com/klab/app/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.klab.app
+
+class JVMPlatform : Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
